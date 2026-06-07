f143c0234ac2d515568e857606883ebaff2d6a4e
Sazid <sazid.dataalchemy@gmail.com>
2026-06-07 19:57:36 +0600

    update

--- src/loaders/practice.py
+++ src/loaders/practice.py
@@ -1,8 +1,47 @@
 import csv
-import pandas as pd
+
 from pathlib import Path
 import re
 import psycopg2
+
+
+blk = [1,5,7,2,2,3,4,5,6,7,8,9]
+blk1 = set(blk)
+
+print(blk1.add(65))
+print(re.__path__)
+
+
+
+
+
+
+
+
+
+
+blk = [1,2,3,4,5,6,7,8,9]
+r=-1
+
+
+for i in range(int(len(blk)/2)):
+    if blk[i]==4:
+        print(f"got, iteration:{i+1}")
+        break
+    elif  blk[r]==4:
+        print(f"got, iteration reverse:{abs(r)}")
+        break
+    r-=1
+ 
+      
+
+
+
+
+
+
+
+
 
 dict ={
     "col1" :["1","2","3","4","5"],
@@ -35,8 +74,8 @@ csv_files = list(landing_folder.glob("*.csv"))
 for csv_file in csv_files:
     with open(csv_file, 'r') as f:
         match = re.match(r"^payments|products|customers|orders|order_items", csv_file.stem)
-        print(match.group(0))
-        print(next(csv.reader(f)))
+        #print(match.group(0))
+        #print(next(csv.reader(f)))
 
         conn = psycopg2.connect(
             host="localhost",
@@ -45,11 +84,14 @@ for csv_file in csv_files:
             user="sazid"
         )
 
-        conn.cursor().execute(
-            "select * from operational_log.bronze_ingest_log;"
+        cur=conn.cursor()
+        cur.execute(
+            "select * from operational_log.bronze_ingest_log where ingestion_id=(select ingestion_id from operational_log.ingestion_id);"
         )
-        print(conn.cursor().fetchall())
+        print(cur.fetchall()[0][0])
         conn.close()
+
+
 
 
 
